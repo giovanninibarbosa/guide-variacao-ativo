54f1053f831472f866c54c12110676bc7272401c
Giovannini Barbosa <giovanninibaarbosa@gmail.com>
2024-01-05 13:42:03 -0300

    feat: adds flutter module to implements on native project.

---
+++ guide-stocks-mobile-app/module_hybrid_flutter/.android/app/src/main/java/tech/barackdafarialima/mobile/hybridflutter/guidestocks/investimentos/module_hybrid_flutter/host/MainActivity.java
@@ -0,0 +1,6 @@
+package tech.barackdafarialima.mobile.hybridflutter.guidestocks.investimentos.module_hybrid_flutter.host;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
